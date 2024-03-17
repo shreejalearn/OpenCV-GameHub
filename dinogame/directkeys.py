0df65affa5fb4d5bf875290d8001e1fa146c3767
Shreeja Das <88001308+shreejalearn@users.noreply.github.com>
2024-03-17 14:55:55 -0400

    Create directkeys.py

---
+++ dinogame/directkeys.py
@@ -0,0 +1,41 @@
+import ctypes
+import time
+
+# Constants
+SendInput = ctypes.windll.user32.SendInput
+SPACE_KEY = 0x39
+
+# Structures
+class KeyBdInput(ctypes.Structure):
+    _fields_ = [("wVk", ctypes.c_ushort),
+                ("wScan", ctypes.c_ushort),
+                ("dwFlags", ctypes.c_ulong),
+                ("time", ctypes.c_ulong),
+                ("dwExtraInfo", ctypes.POINTER(ctypes.c_ulong))]
+
+class Input_I(ctypes.Union):
+    _fields_ = [("ki", KeyBdInput)]
+
+class Input(ctypes.Structure):
+    _fields_ = [("type", ctypes.c_ulong),
+                ("ii", Input_I)]
+
+# Functions
+def PressKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def ReleaseKey(hexKeyCode):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+# Main
+if __name__ == '__main__':
+    PressKey(SPACE_KEY)
+    ReleaseKey(SPACE_KEY)
